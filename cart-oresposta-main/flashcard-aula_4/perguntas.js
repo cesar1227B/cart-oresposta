ed19895cb1a75a2c5e2599e75a1c32d428b12221
CesarGabriel <133375639+cesar1227B@users.noreply.github.com>
2024-10-04 08:20:42 -0300

    Add files via upload

---
+++ cart-oresposta-main/flashcard-aula_4/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital dos EUA?',
+    'A capital dos EUA é Washington, D.C.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
